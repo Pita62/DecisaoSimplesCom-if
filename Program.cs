26636e13f2d0a526ee8a98adda6a41ca854485c0
Pita62 <guaru.guerreiro@gmail.com>
2026-05-15 19:48:15 -0300

    Reaizado exercício Decisão Simples com if.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Recebe a nota da prova 1
+        Console.Write("Digite a nota da prova 1: ");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        // 2. Recebe a nota da prova 2
+        Console.Write("Digite a nota da prova 2: ");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        // 3. Calcule a média das notas
+        double media = (nota1 + nota2) / 2;
+
+        // Exibe a média calculada
+        Console.WriteLine($"Média do aluno: {media}");
+
+        // 4. Verifica se foi aprovado ou reprovado
+        if (media < 5)
+        {
+            Console.WriteLine("O aluno foi reprovado.");
+        }
+        else
+        {
+            Console.WriteLine("O aluno foi aprovado.");
+        }
+    }
+}
